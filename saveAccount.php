87bf437e77487adfc4bef74c7fa4a806c4bf3776
Matt <mdelfierro@algomau.ca>
2025-05-30 22:42:15 +0000

    Quick changes.

--- saveAccount.php
+++ saveAccount.php
@@ -4,13 +4,11 @@ require_once('login.php');
 require_once('user.php');
 // This file will be used to save the account information to the database.
 // A combination of password hashing and salting will be used to store the password. Also, it must be legit with no missing details.
+
 if (password_verify($password, $password) == true){
   $password = password_hash($password, PASSWORD_DEFAULT);
   $username = hash('sha256', $username);
+  $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
   header("Location: /index.php");
-    }  else  {
-      echo "Password is not valid, try again.";
-    }
-
-$query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+} 
 ?>
